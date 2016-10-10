79dc7135df20a2efb76876a0d355d6d2be89da80
Glaucia Lemos <glaucia.lemos@oi.net.br>
2016-10-10 11:23:50 -0300

    Comitando para poder realizar o hash do comit '6c234df' com o objetivo de modificar as alterações do projeto para o módulo 7

--- Modulo 6/src/components/app.js
+++ Modulo 6/src/components/app.js
@@ -9,16 +9,16 @@ var RouteHandler = require('react-router').RouteHandler;
 $ = jQuery = require('jquery');
 
 var App = React.createClass({
-    render: function() {
-            return (
-                <div>
-                    <Header/>
-                    <div className="container-fluid">
-                        <RouteHandler/>
-                    </div>
-                </div>
-            );
-        }
+	render: function() {
+		return (
+			<div>
+				<Header/>
+				<div className="container-fluid">
+					<RouteHandler/>
+				</div>
+			</div>
+		);
+	}
 });
 
 module.exports = App;
